3739caac54640853330e7d7f47191d70dc91e554
zhengcg <1083466294@qq.com>
2018-01-15 10:25:42 +0800

    新提交

--- pages/index/index.js
+++ pages/index/index.js
@@ -1,54 +1,63 @@
 //index.js
 //获取应用实例
-const app = getApp()
-
+var app = getApp()
 Page({
-  data: {
-    motto: 'Hello World',
-    userInfo: {},
-    hasUserInfo: false,
-    canIUse: wx.canIUse('button.open-type.getUserInfo')
+  data:{
+    labels:[
+      {id:1,text:'html5'},
+      {id:2,text:'javascript'},
+      {id:3,text:'css'},
+      {id:4,text:'css3'},
+      {id:5,text:'bootstrap'},
+      {id:6,text:'nodejs'},
+      {id:7,text:'mongodb'},
+      {id:8,text:'json'},
+      {id:9,text:'ajax'},
+      {id:10,text:'jquery'},
+      {id:11,text:'angularjs'},
+      {id:12,text:'vuejs'}
+      ]
+
   },
-  //事件处理函数
-  bindViewTap: function() {
-    wx.navigateTo({
-      url: '../logs/logs'
-    })
+  onLoad:function(options){
+    // 生命周期函数--监听页面加载
+
   },
-  onLoad: function () {
-    if (app.globalData.userInfo) {
-      this.setData({
-        userInfo: app.globalData.userInfo,
-        hasUserInfo: true
-      })
-    } else if (this.data.canIUse){
-      // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
-      // 所以此处加入 callback 以防止这种情况
-      app.userInfoReadyCallback = res => {
-        this.setData({
-          userInfo: res.userInfo,
-          hasUserInfo: true
-        })
-      }
-    } else {
-      // 在没有 open-type=getUserInfo 版本的兼容处理
-      wx.getUserInfo({
-        success: res => {
-          app.globalData.userInfo = res.userInfo
-          this.setData({
-            userInfo: res.userInfo,
-            hasUserInfo: true
-          })
-        }
-      })
+  onReady:function(){
+    // 生命周期函数--监听页面初次渲染完成
+
+  },
+  onShow:function(){
+    // 生命周期函数--监听页面显示
+
+  },
+  onHide:function(){
+    // 生命周期函数--监听页面隐藏
+
+  },
+  onUnload:function(){
+    // 生命周期函数--监听页面卸载
+
+  },
+  onPullDownRefresh: function() {
+    // 页面相关事件处理函数--监听用户下拉动作
+
+  },
+  onReachBottom: function() {
+    // 页面上拉触底事件的处理函数
+
+  },
+  onShareAppMessage: function() {
+    // 用户点击右上角分享
+    return {
+      title: 'title', // 分享标题
+      desc: 'desc', // 分享描述
+      path: 'path' // 分享路径
     }
   },
-  getUserInfo: function(e) {
-    console.log(e)
-    app.globalData.userInfo = e.detail.userInfo
-    this.setData({
-      userInfo: e.detail.userInfo,
-      hasUserInfo: true
+  gotoList:function(e){
+    wx.navigateTo({
+      url: '../list/list?id='+e.currentTarget.dataset.id
     })
   }
 })
